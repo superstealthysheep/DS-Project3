e4c0b9150f6e91fbaae4ef5dc9b40bddf594f270
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2026-05-06 18:43:27 -0500

    make service node into grpc server

--- service-node/service-node.py
+++ service-node/service-node.py
@@ -1,6 +1,38 @@
+import os
+from concurrent import futures
+
+import grpc
+
+import project3_pb2 as p3
+import project3_pb2_grpc as p3_grpc
+
+HOSTNAME = os.environ.get("HOSTNAME", "service-node")
+PORT = os.environ.get("PORT", "50051")
+DATA = {}
+
+
+class ServiceNode(p3_grpc.ReplicaNodeServiceServicer):
+    def S_CreateItem(self, request: p3.CreateItemRequest, context: grpc.ServicerContext) -> p3.CreateItemResponse:
+        return p3.CreateItemResponse(
+            success=False,
+            item=None,
+        )
+
+    def S_GetItem(self, request: p3.GetItemRequest, context: grpc.ServicerContext) -> p3.GetItemResponse:
+        return p3.GetItemResponse(
+            success=False,
+            item=None,
+        )
+
+def serve():
+    print("dummy service node")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    p3_grpc.add_ServiceNodeServiceServicer_to_server(ServiceNode(), server)
+    server.add_insecure_port(f"[::]:{PORT}")
+    server.start()
+    print(f"storage {HOSTNAME} listening on {PORT}", flush=True)
+    server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    import time
-    time.sleep(15)
-    print("dummy service node")
+    serve()
